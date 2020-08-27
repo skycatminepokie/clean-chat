2f0bf24a2b99e56cf65f1af1d47361cc788c3ca7
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2020-08-27 14:58:55 -0700

    Great progress

--- src/main/java/com/skycat/cleanchat/ChatHandler.java
+++ src/main/java/com/skycat/cleanchat/ChatHandler.java
@@ -4,7 +4,7 @@ import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ChatHandler {
-    String[] filteredWords = {"p me","visit me","<!>","<>","ending soon"};
+    String[] filteredWords = {"p me","visit me","<!>","<>","ending soon","-=-=", "[!]", "will show pots", "~~", "****", "!!!!!!!", "Paying for", "/.ah", "on my ah", "in my ah", "go quick", "selling", "buying", "want to buy"};
     int messagesRemoved = 0;
     @SubscribeEvent
     public void onChatMessageRecived(ClientChatReceivedEvent event) {
@@ -13,6 +13,7 @@ public class ChatHandler {
             if (message.toLowerCase().contains(s)) {
                 event.setCanceled(true);
                 messagesRemoved++;
+                System.out.println("Removed the following message: " + message);
                 System.out.println("REMOVED "+messagesRemoved+ " MESSAGES SO FAR");
                 break;
             }
